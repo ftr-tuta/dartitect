b77b5562b198eb3bd1f315839a7c819234afbc4f
ftr <ftr@tuta.com>
2026-08-28 20:33:26 -0400

    fix: refresh workmanager macOS registration

--- examples/thin_consumer_canary/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/thin_consumer_canary/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import workmanager_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  WorkmanagerPlugin.register(with: registry.registrar(forPlugin: "WorkmanagerPlugin"))
 }
